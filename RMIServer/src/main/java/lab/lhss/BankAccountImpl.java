3f001ab9532ed72f864b250dd8c367fe8ac1976a
Lucas Henrique <lucas.henrique0023@gmail.com>
2022-04-07 01:32:41 -0300

    Adjustments to reference Interface instead Implementation

--- RMIServer/src/main/java/lab/lhss/BankAccountImpl.java
+++ RMIServer/src/main/java/lab/lhss/BankAccountImpl.java
@@ -1,23 +1,25 @@
 package lab.lhss;
 
+import java.rmi.RemoteException;
+
 public class BankAccountImpl implements BankAccount {
 
     private double balance;
 
-    public void withdraw(double amount) {
+    public void withdraw(double amount) throws RemoteException {
         this.balance -= amount;
     }
 
-    public void deposit(double amount) {
+    public void deposit(double amount) throws RemoteException {
         this.balance += amount;
     }
 
-    public void transfer(double amount, BankAccountImpl anotherAccount) {
+    public void transfer(double amount, BankAccount anotherAccount) throws RemoteException {
         withdraw(amount);
         anotherAccount.deposit(amount);
     }
 
-    public double getBalance() {
+    public double getBalance() throws RemoteException {
         return this.balance;
     }
 
